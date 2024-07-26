be45712b8eaab386afb5bc35bbc0628e5f70dae7
Snehal-Prajapati <snehal8prajapati@gmail.com>
2024-07-26 14:04:51 +0530

    Bottom | Side bar code + index.ts ffile for file route

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+'use client'
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function Bottombar() {
+  const pathname = usePathname();
+
   return (
-    <div>
-      <h1>Bottombar</h1>
-    </div>
-  )
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              ></Image>
+
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
