89a939875f06c316f762dcb5ae34ba8f0f2fb814
Snehal-Prajapati <snehal8prajapati@gmail.com>
2024-09-02 13:58:42 +0530

    Comment module done

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -34,9 +34,27 @@ export default async function page({ params }: { params: { id: string } }) {
 
       <div className="mt-7">
         <Comment
-        threadId = {thread.id}
-        currentUserImg = {user.imageUrl}
-        currentUserId = {JSON.stringify(userInfo._id)} />
+          threadId={thread.id}
+          currentUserImg={userInfo.image}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user?.id || ""}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   );
--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -17,6 +17,8 @@ import { usePathname, useRouter } from "next/navigation";
 
 // import { updateUser } from "@/lib/actions/user.actions";
 import { CommentValidation } from "@/lib/validations/thread";
+import Image from "next/image";
+import { addCommentToThread } from "@/lib/actions/thread.actions";
 // import { createThread } from "@/lib/actions/thread.actions";
 
 interface Props {
@@ -30,56 +32,53 @@ export default function Comment({
   currentUserImg,
   currentUserId,
 }: Props) {
-    const router = useRouter();
+  const router = useRouter();
   const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(CommentValidation),
     defaultValues: {
       thread: "",
-      accountId: userId,
     },
   });
 
   const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
-    await createThread({ 
-      text: values.thread,
-      author: userId,
-      communityId: null,
-      path: pathname
-     });
+    await addCommentToThread(threadId, values.thread, JSON.parse(currentUserId), pathname);
 
-     router.push("/");
+    form.reset();
   };
 
   return (
     <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="mt-10 flex flex-col justify-start gap-10"
-      >
+      <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form">
         <FormField
           control={form.control}
           name="thread"
           render={({ field }) => (
-            <FormItem className="flex flex-col w-full gap-3">
-              <FormLabel className="text-base-semibold text-light-2">
-                Content
+            <FormItem className="flex items-center w-full gap-3">
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt="Profile image"
+                  width={48}
+                  height={48}
+                  className="rounded-full object-cover"
+                />
               </FormLabel>
-              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
-                <Textarea
-                  rows={15}
-                  className="account-form_input no-focus"
+              <FormControl className="border-none bg-transparent">
+                <Input
+                  type="text"
+                  placeholder="Comment..."
+                  className="no-focus text-light-1 outline-none"
                   {...field}
                 />
               </FormControl>
-              <FormMessage />
             </FormItem>
           )}
         />
 
-        <Button type="submit" className="bg-primary-500">
-          Post Thread
+        <Button type="submit" className="comment-form_btn">
+          Reply
         </Button>
       </form>
     </Form>
