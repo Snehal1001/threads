c4f940d1362504d9d5133244a1882023814d40e3
Snehal-Prajapati <snehal8prajapati@gmail.com>
2024-09-16 18:03:13 +0530

    Activity start

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,8 +1,13 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 export default async function page() {
-    return (
-      <section>
-          <h1 className="head-text mb-10">Activity</h1>
-      </section>
-    )
-  }
-  
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+}
